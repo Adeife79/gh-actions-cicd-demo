92092c7929ba932bec4c36088313c69d060dfadb
Adeife79 <ifeoluwaaderoju6@gmail.com>
2026-04-21 09:18:44 -0700

    add remote backend

--- terraform-infra/provider.tf
+++ terraform-infra/provider.tf
@@ -1,11 +1,11 @@
-#terraform { 
-#
- #   backend "s3" {
- #       bucket = "gh-actions-demo1-tf-state-bucket"
- #       key = "terraform.tfstate"
- #       region = "us-west-2"
- #   }
-#}
+terraform { 
+
+    backend "s3" {
+        bucket = "gh-actions-demo1-tf-state-bucket"
+        key = "terraform.tfstate"
+        region = "us-west-2"
+    }
+}
 
 
 provider "aws" {
