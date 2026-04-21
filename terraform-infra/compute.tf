5e9754b9852a6ab910dcbfcf30eace9a221bd6f9
Adeife79 <ifeoluwaaderoju6@gmail.com>
2026-04-21 14:53:43 -0700

    update compute.tf file

--- terraform-infra/compute.tf
+++ terraform-infra/compute.tf
@@ -1,12 +1,12 @@
-resource "tls_private_key" "ec2_key" {
-    algorithm = "RSA"
-    rsa_bits = 4096
-}
+#resource "tls_private_key" "ec2_key" {
+#    algorithm = "RSA"
+#    rsa_bits = 4096
+#}
 
-resource "aws_key_pair" "app_key" {
-    key_name = "app-keypair"
-    public_key = tls_private_key.ec2_key.public_key_openssh
-}
+#resource "aws_key_pair" "app_key" {
+#    key_name = "app-keypair"
+#    public_key = tls_private_key.ec2_key.public_key_openssh
+#}
 
 resource "aws_instance" "app" {
     ami = var.app_ami
@@ -14,7 +14,7 @@ resource "aws_instance" "app" {
     subnet_id = aws_subnet.app_subnet.id
     security_groups = [aws_security_group.sg.id]
 
-    key_name = aws_key_pair.app_key.key_name
+    key_name = app-ssh-key
 
     tags = {
         Name = "app-instance"
